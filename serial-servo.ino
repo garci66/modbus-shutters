94ce9b958ad9d7302e07165d72d85fb7a4214f73
garci66 <garci66@gmail.com>
2020-08-24 18:14:57 -0300

    using native pwm

--- serial-servo.ino
+++ serial-servo.ino
@@ -5,16 +5,18 @@
 #define DIRECTION_UP 1
 #define DIRECTION_DN -1
 #define DIRECTION_STOP 0
-#define FULL_SPEED_DELTA 85
+#define FULL_SPEED_DELTA 126
 #define DIRECTION_MODE 0
 #define TARGET_MODE 1
 
-Servo myservo;
-int servoVal=0;
-int servoDelta=0;
-int zeroPoint=90;
+//Servo myservo;
+uint8_t servoVal=0;
+int8_t servoDelta=0;
+uint8_t zeroPoint=184;
 int positionCounter=0;
 int falseCounter=0;
+uint8_t prescaler=  _BV(CS22) | _BV(CS20) ;
+bool invertPwm=true;
 
 
 int operMode=DIRECTION_MODE;
@@ -32,8 +34,13 @@ long printMillis=0;
 int potread=0;
 char keyRead;
 void setup() {
+  pinMode(SERVOPIN, OUTPUT);
+  pinMode(11, OUTPUT);
+  TCCR2A = _BV(COM2B1) | (_BV(COM2B0)&& invertPwm) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = prescaler;
+  
   // put your setup code here, to run once:
-  myservo.attach(SERVOPIN);
+  //myservo.attach(SERVOPIN);
   Serial.begin(115200);
   //while (!Serial);
   Serial.println("Finished init");
@@ -78,7 +85,17 @@ void loop() {
     Serial.print(" ");
     Serial.print(servoVal);
     Serial.print(" ");
-    Serial.println(zeroPoint);
+    Serial.print(zeroPoint);
+    Serial.print(" ");
+    Serial.print(TCCR2A,BIN);
+    Serial.print(" ");
+    Serial.print(TCCR2B,BIN);
+    Serial.print(" ");
+    Serial.print(OCR2A,BIN);
+    Serial.print(" ");
+    Serial.println(OCR2B,BIN);
+
+    
     printMillis=millis();
   }
   
@@ -103,6 +120,18 @@ void loop() {
         break; 
       case '3':
         servoVal=zeroPoint+FULL_SPEED_DELTA;
+        break; 
+      case '5':
+        TCCR2B = 4;
+        break; 
+      case '6':
+        TCCR2B = 5;
+        break; 
+      case '7':
+        TCCR2B = 6;
+        break; 
+      case '8':
+        TCCR2B = 7;
         break; 
       case 'p':
         power=!power;
@@ -139,13 +168,22 @@ void loop() {
       case 'x':
         targetPosition=positionCounter;
         break;  
+      case 'i':
+        invertPwm=!invertPwm;
+        if (invertPwm){
+          TCCR2A = _BV(COM2B1) | _BV(COM2B0) | _BV(WGM21) | _BV(WGM20);  
+        } else {
+          TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);  
+        }
+        
+        break; 
     }
-    Serial.print("servoDelta:");
-    Serial.println(servoDelta);
-    Serial.print("Accel Interval:");
-    Serial.print(accelInterval);
-    Serial.print("targetApproach:");
-    Serial.println(targetApproach);
+//    Serial.print("servoDelta:");
+//    Serial.println(servoDelta);
+//    Serial.print("Accel Interval:");
+//    Serial.print(accelInterval);
+//    Serial.print("targetApproach:");
+//    Serial.println(targetApproach);
   }
   if (servoDelta >0){
     currentDirection=DIRECTION_UP;
@@ -157,9 +195,14 @@ void loop() {
   servoVal=zeroPoint+servoDelta;
 
   if (power){
-    myservo.write(servoVal);
+      //analogWrite(SERVOPIN,servoVal);
+      OCR2B = servoVal;
+ //     OCR2A = servoVal;
   } else {
-    myservo.writeMicroseconds(0);
+      //digitalWrite(SERVOPIN,1);
+      //analogWrite(SERVOPIN,1);
+      OCR2B = 0xff;
+ //     OCR2A = 0xff;
   }
 
 
